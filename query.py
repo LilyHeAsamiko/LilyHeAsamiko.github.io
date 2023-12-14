d4e11b55a8d402de72a8bd1d0e2cdcbcf8991248
Qin He(Lily) <45560662+LilyHeAsamiko@users.noreply.github.com>
2023-12-14 15:19:17 +0800

    Update query.py

--- query.py
+++ query.py
@@ -14,4 +14,5 @@ def query():
         return render_template('query.html', dict_return = dict_return)
 
 if __name__ == '__main__':
-   app.run(debug = True)
+#   app.run(debug = True)
+    app.run(host='0.0.0.0', port=9095,threaded=True)
